00d0d7979c94dbb2f8014a97fb6697a905e5306a
Eiyarrr <eiyar@eiyar.com>
2026-05-17 07:37:20 +0000

    feat: added single_player implementation

--- single_player.py
+++ single_player.py
@@ -1,2 +1,22 @@
+import random
+
 def single_player(previous_maps, map_pool):
-    print()
+    # init hashmap with counts, random float is added to prevent ties
+    # from being broken by alphanumeric ordering
+    map_counts = {}
+    for m in map_pool:
+        map_counts[m] = random.random()
+    
+    # fill hashmap with number of previous maps
+    for m in previous_maps:
+        map_counts[m] += 1
+
+    # create arr of hashmap items
+    # sort + reverse for least played maps at the front
+    arr = []
+    for m, c in map_counts.items():
+        arr.append([c, m])
+    arr.sort()
+    arr.reverse()
+    
+    return arr
