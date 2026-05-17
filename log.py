0e66c4b95e5b4991092b7dd67c29ec7edc2a48b5
Eiyarrr <eiyar@eiyar.com>
2026-05-17 09:22:51 +0000

    feat: added json logging

--- log.py
+++ log.py
@@ -2,4 +2,12 @@ import json
 
 
 def log(map_pool, s_hist, s_next, m_hist, m_next):
-    print()
+    data = []
+    data.append(("MAP POOL", map_pool))
+    data.append(("S_HIST", s_hist))
+    data.append(("S_HIST", s_next))
+    data.append(("M_HIST", m_hist))
+    data.append(("M_HIST", m_next))
+    
+    file = open("log.json", "w")
+    json.dump(data, file, indent=4)
