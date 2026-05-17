9b68c5afba2ee2b52a2948930ee08aa7527ca8ad
Eiyarrr <eiyar@eiyar.com>
2026-05-17 09:15:53 +0000

    feat: added generation for multiplayer

--- main.py
+++ main.py
@@ -18,23 +18,36 @@ map_pool = [
 ]
 
 
-def gen_history(count, multiplayer):
-    if not multiplayer:
-        maps = []
+def s_gen_hist(count):
+    maps = []
+    for _ in range(count):
+        rand = random.randint(0, 10)
+        maps.append(map_pool[rand])
+
+    return maps
+
+
+def m_gen_hist(count, players):
+    maps = []
+    for _ in range(players):
+        player_maps = []
         for _ in range(count):
             rand = random.randint(0, 10)
-            maps.append(map_pool[rand])
+            player_maps.append(map_pool[rand])
+        maps.append(player_maps)
 
-        return maps
+    return maps
 
 
 def main():
-    s_hist = gen_history(20, False)
-    m_hist = 0
+    s_hist = s_gen_hist(20)
+    m_hist = m_gen_hist(20, 2)
     s_next = single_player(s_hist, map_pool)
     m_next = multiplayer(m_hist, map_pool)
 
     print(s_hist)
+    print()
+    print(m_hist)
     print()
     print(s_next)
     print()
